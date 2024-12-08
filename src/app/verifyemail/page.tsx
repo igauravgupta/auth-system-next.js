fafcacce0fd539b37dd517b9ec1e1140b02204fa
Gaurav Gupta <itisgauravgupta@gmail.com>
2024-12-08 13:15:15 +0530

    react-suspense

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 
 export default function VerifyEmail() {
   const searchParams = useSearchParams();
